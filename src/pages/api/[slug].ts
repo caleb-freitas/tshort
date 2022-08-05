d43dea9ac57b8027d8698963d0198bcdd0a1a6c8
caleb-freitas <calebfreitas@tutanota.com>
2022-08-05 15:35:23 -0300

    feat: added page redirection

---
+++ src/pages/api/[slug].ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { prisma } from "../../db/prisma-client"
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const slug = req.query["slug"]
+
+  if (!slug || typeof slug !== "string") {
+    res.statusCode = 404
+    res.send(JSON.stringify({ message: "please, use with a slug" }))
+    return
+  }
+
+  const data = await prisma.shortLink.findFirst({
+    where: {
+      slug: {
+        equals: slug
+      }
+    }
+  })
+
+  if (!data) {
+    res.statusCode = 404
+    res.send(JSON.stringify({ message: "slug not found" }))
+    return
+  }
+
+  return res.redirect(data.url)
+}
